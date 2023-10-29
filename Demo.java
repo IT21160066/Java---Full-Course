5b3f894e47970a5dc3474bd9b5bea19bb76a303e
Pasindu Perakum <it21160066my.sliit.lk>
2023-10-29 12:54:48 +0530

    logical operators

--- Demo.java
+++ Demo.java
@@ -2,12 +2,19 @@ public class Demo {
 
     public static void main(String args[]) {
 
-        int x = 6;
-        int y = 6;
+        // int x = 6;
+        // int y = 6;
+        // int a = 5;
+        // int b = 9;
 
         // boolean result = x < y;
-        boolean result = x != y;
-        System.out.println(result);
+        // boolean result = x != y;
+        // System.out.println(result);
+
+        // boolean result = x > y || a > b;
+        // System.out.println(result);
+
+        // conditional statements
 
     }
 }
