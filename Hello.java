73eed53885e32c278f7659e6218e9f94d3a2abb3
Pasindu Perakum <it21160066my.sliit.lk>
2023-10-29 09:44:37 +0530

    type conversion, casting, promotion

--- Hello.java
+++ Hello.java
@@ -1,11 +1,57 @@
 class Hello {
-    public static void main(String a[]) {
+    public static void main(String args[]) {
 
-        int num1 = 3;
-        int num2 = 5;
-        int result = num1 + num2;
-        System.out.println(result);
+        // data types
+
+        // int num1 = 3;
+        // int num2 = 5;
+        // int result = num1 + num2;
+
+        // double marks = 6.5;
+        // float num = 5.6f;
+        // byte by = 127;
+        // long l = 5854l;
+        // System.out.println(result);
 
         // System.out.println("hello");
+
+        // literals
+
+        // int num1 = 0b101;
+        // System.out.println(num1);
+
+        // int num2 = 0x7E;
+        // System.out.println(num2);
+
+        // int num3 = 10_000_000;
+        // System.out.println(num3);
+
+        // double num4 = 12e10;
+        // System.out.println(num4);
+
+        // char c = 'a';
+        // c++;
+        // System.out.println(c);
+
+        // byte b = 127;
+        // int a = b;
+        // System.out.println(a);
+
+        // int a = 257;
+        // byte k = (byte) a;
+
+        // float m = 5.6f;
+        // int t = (int) m;
+
+        // System.out.println(t);
+
+        // type promotion
+
+        byte a = 10;
+        byte b = 20;
+
+        int result = a * b;
+        System.out.println(result);
+
     }
 }
