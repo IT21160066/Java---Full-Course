6b10bb016eac1f63f1d56bd2bde7225eba9a4d04
Pasindu Perakum <it21160066my.sliit.lk>
2024-01-05 10:50:02 +0530

    changes

--- Demo.java
+++ Demo.java
@@ -1,3 +1,10 @@
+// class Student{
+
+//     int rollno;
+//     String name;
+//     int marks;
+// }
+
 public class Demo {
 
     public static void main(String args[]) {
@@ -16,16 +23,113 @@ public class Demo {
 
         // conditional statements
 
-        int x = 8;
-        int y = 5;
-        int z = 7;
+        // int x = 8;
+        // int y = 5;
+        // int z = 7;
 
-        if (x > y)
-            System.out.println("Hello");
-        if (x > z)
-            System.out.println("See you");
-        else
-            System.out.println("Bye");
+        // if (x > y)
+        //     System.out.println("Hello");
+        // if (x > z)
+        //     System.out.println("See you");
+        // else
+        //     System.out.println("Bye");
+
+        // int nums[] = {3,7,2,4};
+        // nums[0] = 8;
+
+        // for(int i = 0; i <= nums.length - 1; i++){
+
+        //     System.out.println(nums[i]);
+        // }
+
+        // int nums[][] = new int[3][4];
+
+        // for(int i = 0; i <= 2; i++){
+        //     for(int j = 0; j <= 3; j++){
+        //         nums[i][j] = (int) (Math.random() * 10);
+        //     }
+        // }
+
+        // for(int i = 0; i <= 2; i++){
+        //     for(int j = 0; j <= 3; j++){
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int n[]: nums){
+        //     for(int m: n){
+        //         System.out.print(m + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int nums[][] = new int[3][]; //jagged array
+
+        // nums[0] = new int[3];
+        // nums[1] = new int[4];
+        // nums[2] = new int[2];
+
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = 0; j < nums[i].length; j++){
+        //         nums[i][j] = (int) (Math.random() * 10);
+        //     }
+        // }
+
+        // for(int n[]: nums){
+        //     for(int m: n){
+        //         System.out.print(m + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Student s1 = new Student();
+        // s1.rollno = 1;
+        // s1.name = "kamal";
+        // s1.marks = 54;
+
+        // Student s2 = new Student();
+        // s2.rollno = 2;
+        // s2.name = "saman";
+        // s2.marks = 78;
+
+        // //array hold student references
+        // Student students[] = new Student[2];
+        // students[0] = s1;
+        // students[1] = s2;
+
+        // for(int i = 0; i < students.length; i++){
+
+        //     System.out.println("Name : "+students[i].name+ ", Roll No : "
+        //     +students[i].rollno+ " Marks are : " +students[i].marks);
+        // }
+
+        // for(Student n: students){
+        //     System.out.println("Name : "+n.name+ ", Roll No : "
+        //      +n.rollno+ " Marks are : " +n.marks);
+        // }
+
+        // int nums[] = new int[6];
+        // nums[0] = 4;
+        // nums[1] = 8;
+        // nums[2] = 3;
+        // nums[3] = 9;
+
+        // for(int i = 0; i < nums.length; i++){
+        //     System.out.println(nums[i]);
+        // }
+
+        // for(int m: nums){
+        //     System.out.println(m);
+        // }
+
+        // String name = new String("kamal");
+        // System.out.println(name);
+
+        // StringBuffer sb = new StringBuffer("kamal");
+        // // System.out.println(sb.capacity());
+        // // System.out.println(sb.length());
+        // sb.append("Perera");
 
     }
 }
--- oop.java
+++ oop.java
@@ -1,9 +1,12 @@
 class Calculator{
 
+    public int calculateValue(int num1, int num2, int num3){
+
+        return num1 +  num2 + num3;
+    }
     public int calculateValue(int num1, int num2){
 
-        int result = num1 +  num2;
-        return result;
+        return num1 +  num2;
     }
 }
 
@@ -13,10 +16,41 @@ public class oop {
 
         int n1 = 5;
         int n2 = 4;
+        int n3 = 6;
         
         Calculator calc = new Calculator();
-        int result = calc.calculateValue(n1, n2);
+
+        int result = calc.calculateValue(n1, n2, n3);
         System.out.println(result);
+
+        int value = calc.calculateValue(n1, n2);
+        System.out.println(value);
     }
     
 }
+
+// class Computer{
+    
+//     public void playMusic(){
+//         System.out.println("music");
+//     }
+
+//     public String getMeAPen(int cost){
+
+//         if(cost >= 10)
+//             return "Pen";
+//         else
+//             return "Invalid";
+//     }
+// }
+
+// public class oop {
+
+//     public static void main(String args[]) {
+
+//         Computer computer = new Computer();
+//         computer.playMusic();
+//         String valueOfpen = computer.getMeAPen(25);
+//         System.out.println(valueOfpen);
+//     }
+// }
