ee48a87ae197df02ad083f2cfda5387913b0fd07
Pasindu Perakum <it21160066my.sliit.lk>
2024-01-07 09:50:26 +0530

    changes

---
+++ Object.java
@@ -0,0 +1,14 @@
+class A {
+    public void show() {
+        System.out.println("A");
+    }
+}
+
+public class Object {
+
+    public static void main(String args[]) {
+        A a = new A();
+        a.show();
+    }
+
+}
